6d97de1aa40449bc6bbfad1e6265dbd428ad22ce
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-11 21:03:08 -0600

    added navbar

--- src/App/App.js
+++ src/App/App.js
@@ -3,31 +3,34 @@ import connection from '../Helpers/data/connection';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth from '../components/Auth/Auth';
 import Listings from '../components/Listings/Listing';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 import './App.scss';
 
 class App extends Component {
   state = {
     authed: false,
-  }
+  };
 
   componentDidMount() {
     connection();
-  }
+  };
 
   isAuthenticated = () => {
     this.setState({authed: true});
-  }
+  };
 
   render() {
-    if(!this.state.authed) {
+    if (!this.state.authed) {
       return (
         <div className="App">
+          <MyNavbar />
           <Auth isAuthenticated={this.isAuthenticated}/>
         </div>
       )
     }
     return (
       <div className="App">
+        <MyNavbar />
         <Listings />
       </div>
     );
