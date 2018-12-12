4317e431f6cbdf56be74ea01a90e4dc45b874334
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-11 20:51:44 -0600

    add auth state

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import connection from '../Helpers/data/connection';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth from '../components/Auth/Auth';
+import Listings from '../components/Listings/Listing';
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({authed: true});
+  }
+
   render() {
+    if(!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      )
+    }
     return (
       <div className="App">
-        <Auth />
+        <Listings />
       </div>
     );
   }
