0f7443686b1434f1894852c01a8915810af31b01
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-11 19:34:40 -0600

    auth setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,40 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import connection from '../Helpers/data/connection';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
+import Auth from '../components/Auth/auth';
+import './App.scss';
 
 class App extends Component {
+  componentDidMont() {
+    connection();
+  }
   render() {
-    console.log(this);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-            <button className='btn btn-danger'>HELP ME</button>
-            <Button
-              tag="a"
-              color="success"
-              size="large"
-              href="http://reactstrap.github.io"
-              target="_blank"
-              >
-                Hello
-            </Button>
-        </header>
+        <Auth />
       </div>
-    );
+    )
   }
 }
 
