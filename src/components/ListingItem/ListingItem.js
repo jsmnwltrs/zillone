da1682ba46d4e0ac624a6b32aedf59cb489cd0de
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-15 12:04:41 -0600

    added button

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import listingShape from '../../Helpers/props/listingShape';
 import formatPrice from '../../Helpers/formatPrice';
+import authRequests from '../../Helpers/data/authRequests';
 import './ListingItem.scss';
 
 class ListingItem extends React.Component {
@@ -10,10 +11,25 @@ class ListingItem extends React.Component {
 
   render() {
     const { listing } = this.props;
+    const uid = authRequests.getCurrentUid();
+
+    const makeButtons = () => {
+      if (listing.uid === uid) {
+        return (
+          <div>
+            <span className="col"><button className="btn btn-default"><i class="far fa-trash-alt"></i></button></span>
+            {/* <span className="col"><button>Edit</button></span> */}
+          </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
+
     return (
       <li className="listing-item text-center">
         <span className="col-7">{listing.address}</span>
         <span className="col-3">{formatPrice(listing.price)}</span>
+        {makeButtons()}
       </li>
     );
   }
