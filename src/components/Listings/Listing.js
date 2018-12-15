f946a787ea4b7a85e1bc32c6fd5a8f08adf40bae
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-15 10:38:38 -0600

    setup listing prop types

--- src/components/Listings/Listing.js
+++ src/components/Listings/Listing.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Listing.scss';
+import PropTypes from 'prop-types';
+import listingShape from '../../Helpers/props/listingShape';
 
 class Listings extends React.Component {
+  static propType = {
+    listings: PropTypes.arrayOf(listingShape),
+  }
+
   render() {
     return (
       <div className="listings col">
