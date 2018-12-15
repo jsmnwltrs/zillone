60efcf41b5761ba8ac451e25fbecad51da4a8eb9
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-15 09:40:19 -0600

    added prop types

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './auth.scss';
 import authRequests from '../../Helpers/data/authRequests';
 
 class Auth extends React.Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.func,
+  }
+
   authenticateUser = (e) => {
     e.preventDefault();
     authRequests.authenticate().then(() => {
