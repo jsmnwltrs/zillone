e98c5ff5adb618a96d3bdefc60a454075956fc37
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-15 12:51:42 -0600

    delete is working

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import listingShape from '../../Helpers/props/listingShape';
 import formatPrice from '../../Helpers/formatPrice';
 import authRequests from '../../Helpers/data/authRequests';
 import './ListingItem.scss';
 
+
 class ListingItem extends React.Component {
   static propTypes = {
     listing: listingShape,
+    deleteSingleListing: PropTypes.func,
+  }
+
+  deleteEvent = (e) => {
+    e.preventDefault();
+    const { deleteSingleListing, listing } = this.props;
+    deleteSingleListing(listing.id);
   }
 
   render() {
@@ -17,7 +26,7 @@ class ListingItem extends React.Component {
       if (listing.uid === uid) {
         return (
           <div>
-            <span className="col"><button className="btn btn-default"><i class="far fa-trash-alt"></i></button></span>
+            <span className="col"><button className="btn btn-default" onClick={this.deleteEvent}><i className="far fa-trash-alt"></i></button></span>
             {/* <span className="col"><button>Edit</button></span> */}
           </div>
         );
